524c81fd87d006b111990e6e5c63dd650bf410c7
Théo Berr <theo.berr@icloud.com>
2025-02-17 08:47:14 +0100

    :bug: fixed database model problem

--- models/comment.py
+++ models/comment.py
@@ -1,11 +1,13 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field, Relationship
+from models.user import User
+from models.project import Project
 
 class Comment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    content: str = Field(index=True)
+    content: str
     author_id: Optional[int] = Field(default=None, foreign_key="user.id")
     project_id: Optional[int] = Field(default=None, foreign_key="project.id")
 
-    project: Optional["Project"] = Relationship(back_populates="comments")
-    author: Optional["User"] = Relationship(back_populates="comments")
+    author: Optional[User] = Relationship(back_populates="comments")
+    project: Optional[Project] = Relationship(back_populates="comments")
